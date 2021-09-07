1f26d1b6c0a5bf715ff8d6f539e372e330456532
prinako <nyankonton122@gmail.com>
2021-09-07 02:20:19 -0300

    frist init of react

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function App() {
+  return (
+    <div className="">
+      <header className="App-header">
+        <p>Hello Chat App</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
