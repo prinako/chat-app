aac93ad2d1999887bb2692cda8b4111e63fb2db3
prinako <nyankonton122@gmail.com>
2021-09-08 15:38:37 -0300

    working on the signup page

--- src/components/Button.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-
-function Button(props){
-    return <button className={props.className}>{props.text}</button>
-}
-
-export default Button;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,27 +1,38 @@
-import React from "react";
-import Button from "./Button";
-import Input from "./Input";
+import React, { useState } from "react";
+import Button from "./Utilities/Button";
+import Input from "./Utilities/Input";
 
 function Login() {
+  const [HaveAccount, setHaveAccount] = useState(true);
+
+  function handleHaveAccount() {
+    setHaveAccount(HaveAccount ? false : true);
+  }
+
   return (
     <div className="container">
       <div className="form-container">
         <div className="form-title">
-          <h1>Login</h1>
+          <h1>{HaveAccount ? "Login" : "Sign Up"}</h1>
         </div>
         <div className="form-wrapper">
           <Input
             className="form-control"
             type="email"
-            placeholder="User Name or Email"
+            placeholder={HaveAccount ? "User Name or ID" : "User Name"}
           />
           <Input
             className="form-control"
             type="password"
             placeholder="Password"
           />
-          <Button className="form-btn mt-2" text="Login" />
-          <a className="mt-2">Create account</a>
+          <Button
+            className="form-btn mt-2"
+            text={HaveAccount ? "Login" : "Create"}
+          />
+          <button className="form-btn-link mt-2" onClick={handleHaveAccount}>
+            {HaveAccount ? "Create account" : "Login"}
+          </button>
         </div>
       </div>
     </div>
