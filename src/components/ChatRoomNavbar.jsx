e263b7267cebead5e271844be79eeb7262f2ba7a
prinako <nyankonton122@gmail.com>
2021-10-20 23:15:23 -0300

    fix back buttom not showing on waid screen

--- src/components/ChatRoomNavbar.jsx
+++ src/components/ChatRoomNavbar.jsx
@@ -18,7 +18,7 @@ export default function ChatRoomNavbar() {
             ? selectConversationIndex("")
             : selectGroupIndex("")
         }
-        className={`btn`}
+        className={`btn menu`}
         text={<ArrowBackIcon />}
       />
       <div className="brand">
